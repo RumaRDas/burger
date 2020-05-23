6f745b180344662ce12a93d80e235953a98fe324
Ruma Das <rumaadl16@gmail.com>
2020-05-24 00:47:54 +0930

    created seedssql insert some data in table

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(  burger_name,devoured)VALUES("Chees Burger",true);
+INSERT INTO burgers(  burger_name,devoured)VALUES("Chicken Burger",true);
+INSERT INTO burgers(  burger_name,devoured)VALUES("Beef Burger",true);
+INSERT INTO burgers(  burger_name,devoured)VALUES("Chicken Royal Burger",false);
+select * from burgers;
