9e287e9d66b8fc6fd5bdc8c0dca24bae28cba1ec
Ruma Das <rumaadl16@gmail.com>
2020-05-25 00:40:57 +0930

    Create all our routes and set up logic within those routes where required

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,11 +5,17 @@ const burger = require("../models/burger.js");
 let router = express.Router();
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.sellectAll(function(data) {
      let burgerObj = {
         burger: data
       };
       console.log(burgerObj);
       res.render("index", burgerObj);
+    });
+  });
+
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne(req.body.name, function(result) {
+      res.json({ id: result.insertId });
     });
   });
