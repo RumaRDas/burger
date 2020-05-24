9c291ccaa2b19b0486843fd7a584c38ecb5f255d
Ruma Das <rumaadl16@gmail.com>
2020-05-25 00:21:24 +0930

    creating function for orm.js

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// * `selectAll()`
+// * `insertOne()`
+// * `updateOne()`
+// Export the ORM object in `module.exports`
+
+var orm = {
+
+
+
+}
+
+module.exports = orm;
